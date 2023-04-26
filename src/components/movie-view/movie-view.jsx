0c78d7dbc1bbbe23e17eef9df62fdc0b91aec863
Tinnkie <Tinnkie@users.noreply.github.com>
2023-04-27 01:32:43 +0200

    update styling to movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -76,37 +76,36 @@ export const MovieView = ({ movies, user, updateUser, token }) => {
         <img src={movie.image} style={{ width: "100%" }} />
       </div>
       <div>
-        <span>Movie Title: </span>
-        <span>{movie.title}</span>
+        <h2>{movie.title}</h2>
       </div>
       <br />
       <div>
-        <span>Description: </span>
+        <span style={{ fontWeight: 'bold' }}>Description: </span>
         <span>{movie.description}</span>
       </div>
       <br />
       <div>
-        <span>Genre: </span>
+        <span style={{ fontWeight: 'bold' }}>Genre: </span>
         <span>{movie.genre.Name}</span>
       </div>
       <br />
       <div>
-        <span>Genre Description: </span>
+        <span style={{ fontWeight: 'bold' }}>Genre Description: </span>
         <span>{movie.genre.Description}</span>
       </div>
       <br />
       <div>
-        <span>Director: </span>
+        <span style={{ fontWeight: 'bold' }}>Director: </span>
         <span>{movie.director.Name}</span>
       </div>
       <br />
       <div>
-        <span>Bio: </span>
+        <span style={{ fontWeight: 'bold' }}>Bio: </span>
         <span>{movie.director.Bio}</span>
       </div>
       <br />
       <div>
-        <span>Birth: </span>
+        <span style={{ fontWeight: 'bold' }}>Birth: </span>
         <span>{movie.director.Birth}</span>
       </div>
       <br />
